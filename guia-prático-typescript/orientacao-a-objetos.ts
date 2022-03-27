b3b83b745517afcac6eba8f728193e710a22962c
hakan <hakan@localhost.localdomain>
2022-03-26 22:10:51 -0300

    TypeScript

---
+++ guia-prático-typescript/orientacao-a-objetos.ts
@@ -0,0 +1,36 @@
+// APESAR DESSE ARQUIVO TER UMA SIMILARIDADE COM O PROJETO BANCO-GIT, ESTE CÓDIGO NÃO TEM NENHUMA LINHA DE CÓDIGO RETIRADA DELE!
+
+class Conta {
+  numeroDaConta: number;
+  titular: string;
+  private saldo: number = 0;
+  constructor(numeroDaConta: number, titular: string, saldo: number) {
+    this.numeroDaConta = numeroDaConta;
+    this.titular = titular;
+    this.saldo = saldo;
+  }
+
+  consultaSaldo(): string {
+    return `O seu saldo atual é ${this.saldo}`;
+  }
+
+  adicionaSaldo(valor: number): void {
+    this.saldo += valor;
+    return console.log(`Saldo adicionado : R$${valor}`);
+  }
+
+  sacarDoSaldo(valor: number): void {
+    this.saldo -= valor;
+    return console.log(`Valor sacado : R$${valor}`);
+  }
+}
+
+// utilizamos a palavra reservada new para instanciar/criar um novo objeto
+const primeiraConta = new Conta(1, "Luiz", 0);
+console.log(primeiraConta);
+primeiraConta.adicionaSaldo(99);
+console.log(primeiraConta);
+primeiraConta.adicionaSaldo(99901);
+console.log(primeiraConta);
+primeiraConta.sacarDoSaldo(11);
+console.log(primeiraConta);
