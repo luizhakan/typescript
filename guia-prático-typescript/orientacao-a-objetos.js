15b87cf9f6348bfee30824f9aac51cd0cb299b07
luizhakan <uefibiosxx@gmail.com>
2022-03-27 15:44:00 -0300

    TypeScript

--- guia-prático-typescript/orientacao-a-objetos.js
+++ guia-prático-typescript/orientacao-a-objetos.js
@@ -1,37 +1,67 @@
 "use strict";
 // APESAR DESSE ARQUIVO TER UMA SIMILARIDADE COM O PROJETO BANCO-GIT, ESTE CÓDIGO NÃO TEM NENHUMA LINHA DE CÓDIGO RETIRADA DELE!
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Conta = void 0;
 class Conta {
-    constructor(numeroDaConta, titular, saldo) {
-        this.numeroDaConta = numeroDaConta;
-        this.titular = titular.toUpperCase();
-        this.saldo = saldo = 0;
-    }
-    consultaSaldo() {
-        return `O seu saldo atual é ${this.saldo}`;
-    }
-    adicionaSaldo(valor) {
-        this.saldo += valor;
-        return console.log(`Saldo adicionado : R$${valor}`);
-    }
-    sacarDoSaldo(valor) {
-        this.saldo -= valor;
-        return console.log(`Valor sacado : R$${valor}`);
-    }
+  constructor(numeroDaConta, titular, saldo) {
+    this._numeroDaConta = Math.floor(Math.random() * 1000) + 1;
+    this.titular = titular.toUpperCase();
+    this._saldo = saldo = 0;
+  }
+  get numeroDaConta() {
+    return this._numeroDaConta;
+  }
+  consultaSaldo() {
+    return this._saldo;
+  }
+  adicionaSaldo(valor) {
+    this._saldo += valor;
+    return console.log(`Saldo adicionado : R$${valor}`);
+  }
+  sacarDoSaldo(valor) {
+    this._saldo -= valor;
+    return console.log(`Valor sacado : R$${valor}`);
+  }
 }
+exports.Conta = Conta;
 class ContaPF extends Conta {
-    constructor(cpf, numeroDaConta, titular, saldo) {
-        super(numeroDaConta, titular, saldo);
-        this.cpf = cpf;
+  constructor(cpf, numeroDaConta, titular, saldo) {
+    super(numeroDaConta, titular, saldo);
+    this.cpf = cpf;
+  }
+  consulta() {
+    return `Saldo atual da conta : ${this.consultaSaldo()}`;
+  }
+  sacar(valor) {
+    if (this.consultaSaldo() > 0 && valor <= this.consultaSaldo()) {
+      console.log(
+        "Saque realizado, valor : " + "R$" + this.sacarDoSaldo(valor)
+      );
+    } else {
+      console.log("Saldo insuficiente para o saque definido");
     }
+  }
+  depositar(valor) {
+    return this.adicionaSaldo(valor);
+  }
 }
 class ContaPJ extends Conta {
-    constructor(cnpj, numeroDaConta, titular, saldo) {
-        super(numeroDaConta, titular, saldo);
-        this.cnpj = cnpj;
-    }
+  constructor(cnpj, numeroDaConta, titular, saldo) {
+    super(numeroDaConta, titular, saldo);
+    this.cnpj = cnpj;
+  }
+  consulta() {
+    return `Saldo atual da conta : ${this.consultaSaldo()}`;
+  }
+  sacar(valor) {
+    this.sacarDoSaldo(valor);
+  }
+  depositar(valor) {
+    return this.adicionaSaldo(valor);
+  }
 }
 // utilizamos a palavra reservada new para instanciar/criar um novo objeto
-const primeiraContaPF = new ContaPF(11122233312, 1, "Luiz Hakan", 0); // o saldo está privado, mas por qual motivo eu coloquei o terceiro parâmetro então? é que no typescript, se temos um constructor com 3 atributos, quando chamamos a classe, devemos usar os 3 atributos
+const primeiraContaPF = new ContaPF(11122233312, 0, "Luiz Hakan", 0); // o saldo está privado, mas por qual motivo eu coloquei o terceiro parâmetro então? é que no typescript, se temos um constructor com 3 atributos, quando chamamos a classe, devemos usar os 3 atributos
+const primeiraContaPJ = new ContaPJ(12123456789012, 0, "João Victor", 0);
+primeiraContaPF.depositar(100);
 console.log(primeiraContaPF);
-const primeiraContaPJ = new ContaPJ(12123456789012, 2, 'João Victor', 0);
-console.log(primeiraContaPJ);
--- guia-prático-typescript/orientacao-a-objetos.ts
+++ guia-prático-typescript/orientacao-a-objetos.ts
@@ -1,47 +1,88 @@
 // APESAR DESSE ARQUIVO TER UMA SIMILARIDADE COM O PROJETO BANCO-GIT, ESTE CÓDIGO NÃO TEM NENHUMA LINHA DE CÓDIGO RETIRADA DELE!
 
-class Conta {
-  numeroDaConta: number;
+export class Conta {
+  private readonly _numeroDaConta: number;
   titular: string;
-  private saldo: number;
+  private _saldo: number;
   constructor(numeroDaConta: number, titular: string, saldo: number) {
-    this.numeroDaConta = numeroDaConta;
+    this._numeroDaConta = Math.floor(Math.random() * 1000) + 1;
     this.titular = titular.toUpperCase();
-    this.saldo = saldo = 0;
+    this._saldo = saldo = 0;
   }
 
-  consultaSaldo(): string {
-    return `O seu saldo atual é ${this.saldo}`;
+  get numeroDaConta(): number {
+    return this._numeroDaConta;
   }
 
-  adicionaSaldo(valor: number): void {
-    this.saldo += valor;
+  protected consultaSaldo(): number {
+    return this._saldo;
+  }
+
+  protected adicionaSaldo(valor: number): void {
+    this._saldo += valor;
     return console.log(`Saldo adicionado : R$${valor}`);
   }
 
-  sacarDoSaldo(valor: number): void {
-    this.saldo -= valor;
+  protected sacarDoSaldo(valor: number): void {
+    this._saldo -= valor;
     return console.log(`Valor sacado : R$${valor}`);
   }
 }
 
-class ContaPF extends Conta{
-    cpf: number;
-    constructor(cpf:number, numeroDaConta:number, titular:string, saldo:number){
-        super(numeroDaConta, titular, saldo)
-        this.cpf = cpf;
+class ContaPF extends Conta {
+  cpf: number;
+  constructor(
+    cpf: number,
+    numeroDaConta: number,
+    titular: string,
+    saldo: number
+  ) {
+    super(numeroDaConta, titular, saldo);
+    this.cpf = cpf;
+  }
+
+  consulta(): string {
+    return `Saldo atual da conta : ${this.consultaSaldo()}`;
+  }
+
+  sacar(valor: number) {
+    if (this.consultaSaldo() > 0 && valor <= this.consultaSaldo()) {
+      console.log(
+        "Saque realizado, valor : " + "R$" + this.sacarDoSaldo(valor)
+      );
+    } else {
+      console.log("Saldo insuficiente para o saque definido");
     }
+  }
+  depositar(valor: number) {
+    return this.adicionaSaldo(valor);
+  }
 }
-class ContaPJ extends Conta{
-    cnpj: number;
-    constructor(cnpj:number, numeroDaConta: number, titular:string, saldo:number){
-        super(numeroDaConta, titular, saldo)
-        this.cnpj = cnpj;
-    }
+class ContaPJ extends Conta {
+  cnpj: number;
+  constructor(
+    cnpj: number,
+    numeroDaConta: number,
+    titular: string,
+    saldo: number
+  ) {
+    super(numeroDaConta, titular, saldo);
+    this.cnpj = cnpj;
+  }
+  consulta(): string {
+    return `Saldo atual da conta : ${this.consultaSaldo()}`;
+  }
+  sacar(valor: number) {
+    this.sacarDoSaldo(valor);
+  }
+  depositar(valor: number) {
+    return this.adicionaSaldo(valor);
+  }
 }
 
 // utilizamos a palavra reservada new para instanciar/criar um novo objeto
-const primeiraContaPF = new ContaPF(11122233312,1, "Luiz Hakan", 0);  // o saldo está privado, mas por qual motivo eu coloquei o terceiro parâmetro então? é que no typescript, se temos um constructor com 3 atributos, quando chamamos a classe, devemos usar os 3 atributos
+const primeiraContaPF = new ContaPF(11122233312, 0, "Luiz Hakan", 0); // o saldo está privado, mas por qual motivo eu coloquei o terceiro parâmetro então? é que no typescript, se temos um constructor com 3 atributos, quando chamamos a classe, devemos usar os 3 atributos
+const primeiraContaPJ = new ContaPJ(12123456789012, 0, "João Victor", 0);
+
+primeiraContaPF.depositar(100);
 console.log(primeiraContaPF);
-const primeiraContaPJ = new ContaPJ(12123456789012, 2, 'João Victor', 0)
-console.log(primeiraContaPJ)
