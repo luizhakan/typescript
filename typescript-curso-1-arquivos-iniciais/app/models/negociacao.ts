67b17afc68471c057e0001ad3f2ae0afa09a75e5
luizhakan <uefibiosxx@gmail.com>
2022-04-06 07:09:14 -0300

    TypeScript

--- typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -1,27 +1,23 @@
 export class Negociacao {
-  private _data: Date;
-  private _quantidade: number;
-  private _valor: number;
-
-  constructor(data: Date, quantidade: number, valor: number) {
-      this._data = data;
-      this._quantidade = quantidade;
-      this._valor = valor;
-  }
+  constructor(
+    private _data: Date,
+    private _quantidade: number,
+    private _valor: number
+  ) {}
 
   get data(): Date {
-      return this._data;
+    return this._data;
   }
 
   get quantidade(): number {
-      return this._quantidade;
+    return this._quantidade;
   }
 
   get valor(): number {
-      return this._valor;
+    return this._valor;
   }
 
   get volume(): number {
-      return this._quantidade * this._valor;
+    return this._quantidade * this._valor;
   }
 }
