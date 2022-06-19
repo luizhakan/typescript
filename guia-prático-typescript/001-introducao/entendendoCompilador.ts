8933cb93928a80e0759d01155590abbb4880cfa2
luizhakan <uefibiosxx@gmail.com>
2022-06-18 23:08:02 -0300

    TypeScript - Introdução 18/06/2022 - Entendendo o como funciona o compilador typescript

---
+++ guia-prático-typescript/001-introducao/entendendoCompilador.ts
@@ -0,0 +1,3 @@
+let languages : Array<string> = [];
+languages.push("TypeScript"); // até aqui tudo certo, o typescript não vai me retornar erro na compilação
+// languages.push(3); // aqui o typescript não aceita, pois o tipo de "languages" é um array de string
