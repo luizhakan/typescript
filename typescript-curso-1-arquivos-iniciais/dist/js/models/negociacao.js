7ad3518f39e4f3e907a48fabd156c8de642e80a1
luizhakan <luizhakan2@protonmail.com>
2022-08-01 10:10:30 -0300

    TypeScript e compilador 01/08/2022 - Neste commit tem o download do typescript (comando usado foi : npm i typescript@4.2) pra acompanhar a versão do projeto. Configuração do compilador e o tsconfig.json. Modificação dos scripts npm no package.json. Modificadores de acesso em typescript.

--- typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
@@ -1,28 +1,20 @@
 export class Negociacao {
-  #data;
-  #quantidade;
-  #valor;
-
-  constructor(data, quantidade, valor) {
-    this.#data = data;
-    this.#quantidade = quantidade;
-    this.#valor = valor;
-  }
-
-  // estes gets me ajudam a ler os valores destas propriedades privadas
-  get data() {
-    return this.#data;
-  }
-
-  get quantidade() {
-    return this.#quantidade;
-  }
-
-  get valor() {
-    return this.#valor;
-  }
-
-  get volume(){
-    return this.#quantidade * this.#valor;
-  }
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+    // estes gets me ajudam a ler os valores destas propriedades privadas
+    get data() {
+        return this._data;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    get volume() {
+        return this._quantidade * this._valor;
+    }
 }
