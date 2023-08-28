093c439d8a418c92baf098fb77744080131fef12
luizhakan <luizhakan2@protonmail.com>
2023-08-27 23:10:28 -0300

    Benefícios da tipagem estática
    Nesta seção eu revisei sobre o tipo implicito any, e como ativar no tsconfig.json o noImplicityAny. tipei tudo, coloquei um valor default no campo de data, e valor.

--- alura/app/app.ts
+++ alura/app/app.ts
@@ -1,4 +1,17 @@
-import { Negociacao } from "./models/negociacao.js";
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.volume);
+const controller = new NegociacaoController();
+const form = document.querySelector(".form");
+let valor: HTMLInputElement = document.querySelector("#valor");
+let data: HTMLInputElement = document.querySelector("#data");
+
+const date: Date = new Date();
+const dataFormatadaComHifen = date.toISOString().split("T")[0];
+valor.value = "1";
+data.value = dataFormatadaComHifen;
+data.focus();
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  controller.adiciona();
+});
---
+++ alura/app/controllers/negociacao-controller.ts
@@ -0,0 +1,34 @@
+import { Negociacao } from "../models/negociacao.js";
+
+export class NegociacaoController {
+  private inputData: HTMLInputElement;
+  private inputQuantidade: HTMLInputElement;
+  private inputValor: HTMLInputElement;
+
+  constructor() {
+    this.inputData = document.querySelector("#data");
+    this.inputQuantidade = document.querySelector("#quantidade");
+    this.inputValor = document.querySelector("#valor");
+  }
+
+  adiciona(): void {
+    const negociacao = this.criaNegociacao();
+    console.log(negociacao);
+    this.limparFormulario();
+  }
+
+  criaNegociacao(): Negociacao {
+    const exp: RegExp = /-/g;
+    const date: Date = new Date(this.inputData.value.replace(exp, ","));
+    const quantidade: number = parseInt(this.inputQuantidade.value);
+    const valor: number = parseFloat(this.inputValor.value);
+    return new Negociacao(date, quantidade, valor);
+  }
+
+  limparFormulario(): void {
+    this.inputData.value = new Date().toISOString().split("T")[0];
+    this.inputQuantidade.value = "1";
+    this.inputValor.value = "1";
+    this.inputData.focus();
+  }
+}
